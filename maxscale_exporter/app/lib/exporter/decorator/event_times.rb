114b64e47875b58a958de6741e90a9d2d8c071bf
Mike Danko <mike@l4m3.com>
2016-10-21 10:52:09 -0400

    fixing upper bound on histogram

--- maxscale_exporter/app/lib/exporter/decorator/event_times.rb
+++ maxscale_exporter/app/lib/exporter/decorator/event_times.rb
@@ -8,17 +8,15 @@ class Exporter
       end
       def executed
         buckets = @query_obj.executed.collect {|x| %Q[#{@metric_name}_executed_seconds_bucket{le="#{x[:le]}"} #{x[:count]}]}
-        inf = [%Q[#{@metric_name}_executed_seconds_bucket{le="+Inf"} #{@query_obj.executed_count}]]
         sum = [%Q[#{@metric_name}_executed_seconds_sum #{@query_obj.executed_sum}]]
         count = [%Q[#{@metric_name}_executed_seconds_count #{@query_obj.executed_count}]]
-        (buckets + inf + sum + count).join("\n")
+        (buckets + sum + count).join("\n").gsub('le="3.0"','le="+Inf"')
       end
       def queued
         buckets = @query_obj.queued.collect {|x| %Q[#{@metric_name}_queued_seconds_bucket{le="#{x[:le]}"} #{x[:count]}]}
-        inf = [%Q[#{@metric_name}_queued_seconds_bucket{le="+Inf"} #{@query_obj.queued_count}]]
         sum = [%Q[#{@metric_name}_queued_seconds_sum #{@query_obj.queued_sum}]]
         count = [%Q[#{@metric_name}_queued_seconds_count #{@query_obj.queued_count}]]
-        (buckets + inf + sum + count).join("\n")
+        (buckets + sum + count).join("\n").gsub('le="3.0"','le="+Inf"')
       end
     end
   end
